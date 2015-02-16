d9d3ab93231b158cd80b92272dbed5e2f07bc07e
Marcin Kumorek <marcin.kumorek@gamesys.co.uk>
2015-02-16 18:43:57 +0000

    Add css, markup, components and app.js

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default React.createClass({
+
+	displayName: 'App',
+
+	render() {
+		return (
+			<section className="container">
+				<h1>FLUX/React.js movie rating app</h1>
+			</section>
+		)
+	}
+
+})
